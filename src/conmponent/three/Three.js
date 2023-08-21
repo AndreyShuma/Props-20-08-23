829e4b00531636b7e1f2227d96aa0f322c5fdb1f
AndreyShuma <0662389612a@gmail.com>
2023-08-21 13:32:46 +0300

    21-08-2023

--- src/conmponent/three/Three.js
+++ src/conmponent/three/Three.js
@@ -4,8 +4,6 @@ function Three (props) {
     // const f1 = () => {props.data2};
     const {data1, data2} = props;
     
-    // console.log('Three data1>>', data1);
-    // console.log('Three data2>>', data2);
     // console.log('Three props2>>', props);
 
     return (
